bad953e41074b1e272cdfbc986a7899548cf0323
Sanchitha Hiremath <sanchitha@gmail.com>
2026-05-03 14:06:23 +0530

    added welcome to jenkins line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello CI/CD - Version 2 and live demo working");
+        System.out.println("Hello CI/CD - Version 2 and live demo working,welcome to jenkins");
     }
 }
